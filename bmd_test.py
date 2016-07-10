3eb5f3dd467b162a469ff4e82df90dd04158dc2b
Jeremías Casteglione <git@jrms.com.ar>
2016-07-10 19:57:47 -0300

    test tpl_utils

--- bmd_test.py
+++ bmd_test.py
@@ -3,5 +3,16 @@ from unittest import TestCase
 import bmd
 
 class TestBMD(TestCase):
+
+    @bmd.tpl_utils
+    def fakeview(self):
+        return dict()
+
     def test_tpl_utils(self):
-        print('lalala')
+        d = self.fakeview()
+        self.assertIsInstance(d, dict)
+
+    def test_tpl_utils_timefmt(self):
+        f = self.fakeview().get('timefmt')
+        t = f('%Y%m%d %H:%M:%S %z', 1468191315)
+        self.assertEqual(t, '20160710 19:55:15 -0300')
