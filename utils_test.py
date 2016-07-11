48a8051e9f69f046f71ad0bb6b24c7832fae71bc
Jeremías Casteglione <git@jrms.com.ar>
2016-07-10 23:20:28 -0300

    test tpl_dict

--- utils_test.py
+++ utils_test.py
@@ -5,7 +5,12 @@ import utils
 
 class TestUtils(TestCase):
 
-    def test_utils_timefmt(self):
+    def test_tpl_dict(self):
+        d = utils.tpl_dict()
+        self.assertIsInstance(d, dict)
+        self.assertEqual(d['timefmt'], utils.timefmt)
+
+    def test_timefmt(self):
         t = utils.timefmt(1468191315)
         self.assertEqual(t, 'Sun, 10 Jul 2016 19:55:15 -0300')
         t = utils.timefmt(1468191315, '%Y%m%d %H:%M:%S %z')
